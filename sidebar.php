f5b2b2661ebaaad629491c4567480d77aa21d72b
devhelalbd <devmohsinali76@gmail.com>
2024-08-23 16:23:59 +0600

    added dynamic media url and split the template and blocks

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<div class="about-menu">
+                <h4>sidebar menu</h4>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <h4>sidebar menu</h4>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
---
+++ template-parts/content-about.php
@@ -0,0 +1,6 @@
+<div class="about-left">
+     <h4>about us</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
+          <a href="about.html" class="btn">read more</a>
+</div>
