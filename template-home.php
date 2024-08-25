b92f257f9c8119d6fea4cc9b8a10df32f582a619
devhelalbd <devmohsinali76@gmail.com>
2024-08-25 23:27:46 +0600

    dynamic home and blog page

---
+++ template-home.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+
+Template Name: Template Home
+
+ */
+
+get_header(); ?>
+
+        <!-- Banner Start Here -->
+        <div class="banner">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bannar.jpg" alt="Banner">
+        </div>
+        <!-- Banner End Here -->
+        
+        <!-- Services Start Here -->
+        <?php get_template_part('template-parts/content', 'services') ?>
+        <!-- Services End Here -->
+        
+        <!-- About Start Here -->
+        <div class="about fix" id="about">
+            
+        <?php get_template_part('template-parts/content', 'about') ?>
+z
+            <!-- Sidebar Start Here -->
+
+            <?php get_sidebar(); ?>
+
+            <!-- Sidebar End Here -->
+
+        </div>
+        <!-- About End Here -->
+
+
+        <!-- Start Footer Here -->
+
+        <?php get_footer(); ?>
+
+        <!-- Footer End Here -->
+
+   
