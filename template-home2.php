e496ea0baf082b4865ccda7245a493f0331e0f34
devhelalbd <devmohsinali76@gmail.com>
2024-09-22 23:38:08 +0600

    add custom field query

--- template-home2.php
+++ template-home2.php
@@ -14,17 +14,45 @@ get_header(); ?>
         $args = array(
             'post_type' => 'post',
             'posts_per_page' => 10,
-            'meta_compare' => '=',
+
+            // query compare in blog post 
+
+            // 'meta_compare' => '=',
             // 'meta_value' => '3',
-            'meta_value' => 'john',
-            'meta_key' => 'name'
+            // 'meta_value' => 'john',
+            // 'meta_key' => 'name'
+
+            // query in blog post using post category
+            // 'category_name' => 'book'
+
+            //meta query
+            'meta_query' => array(
+                'relation' => 'OR',
+                array(
+                    'key' => 'size',
+                    'value' => 'b',
+                    'compare' => '='
+                ),
+                array(
+                    'key' => 'price',
+                    'value' => '100',
+                    'compare' => '<'
+                )
+            )
         );
         $query = new WP_Query($args);
 
         while($query -> have_posts()){
             $query -> the_post();
         ?>
-            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            
+            <div>
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <p>Price: <?php the_field('price'); ?></p>
+                <p>Size: <?php the_field('size'); ?></p>
+                <p>Color: <?php the_field('color'); ?></p>
+
+            </div>
         <?php
         }
     ?>
