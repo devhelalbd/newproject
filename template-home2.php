2d3964f6a431a2c6230214900208a79f58c65f07
devhelalbd <devmohsinali76@gmail.com>
2024-09-22 23:11:31 +0600

    blog post compare

--- template-home2.php
+++ template-home2.php
@@ -13,9 +13,11 @@ get_header(); ?>
     <?php
         $args = array(
             'post_type' => 'post',
-            'posts_per_page' => -1,
-            'order' => 'ASC',
-            'orderby' => 'rand'
+            'posts_per_page' => 10,
+            'meta_compare' => '=',
+            // 'meta_value' => '3',
+            'meta_value' => 'john',
+            'meta_key' => 'name'
         );
         $query = new WP_Query($args);
 
